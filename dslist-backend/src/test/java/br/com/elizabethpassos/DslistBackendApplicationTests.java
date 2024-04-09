babf330c069e94593511f91f8ee85bda23771852
Elizabeth Passos <48329605+Elizabeth37@users.noreply.github.com>
2024-04-09 16:14:18 -0300

    Adicionei arquivos via upload.

---
+++ dslist-backend/src/test/java/br/com/elizabethpassos/DslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elizabethpassos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
